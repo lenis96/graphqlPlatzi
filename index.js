58c60b4411ab1dcd8189213a53a93fe7aa7c61d8
lenis96 <lenisandres5@gmail.com>
2019-02-14 08:32:03 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express=require('express')
+const bodyParser=require('body-parser')
+const {graphqlExpress,graphiqlExpress}=require('graphql-server-express')
+const schema=require('./schema')
+
+const PORT=8080
+const app=express()
+
+app.use(
+    '/graphql',
+    bodyParser.json(),
+    graphqlExpress({
+        schema
+    })
+)
+
+app.use('/graphiql',graphiqlExpress({
+    endpointURL:'/graphql'
+}))
+
+app.listen(PORT,()=>{
+    console.log(`server runnning on port ${PORT}`)
+})
